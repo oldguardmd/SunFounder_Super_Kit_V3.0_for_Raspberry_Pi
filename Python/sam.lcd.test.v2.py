a5f58e4ea4b3f5ccd2edac2c14144fa01c26a80d
oldguardmd <oldguard@msn.com>
2021-02-20 16:38:58 -0500

    Fixed self variable

--- Python/sam.lcd.test.v2.py
+++ Python/sam.lcd.test.v2.py
@@ -3,7 +3,7 @@ from time import sleep
 #We are using GPIO port numbers instead of pin numbers.
 
 class LCD:
-    def __init__(pin_e=15, pin_rs=13, pin_list=[22,18,16,12]):
+    def __init__(self, pin_e=15, pin_rs=13, pin_list=[22,18,16,12]):
         import RPi.GPIO as GPIO
         self.GPIO = GPIO
         GPIO.setmode(GPIO.BOARD)
@@ -11,7 +11,7 @@ class LCD:
         self.pin_e = pin_e
         self.pin_list = pin_list
 
-        self.full_pin_list = self.pin_list[]
+        self.full_pin_list = self.pin_list[:]
         self.full_pin_list.append(self.pin_e)
         self.full_pin_list.append(self.pin_rs)
 
