a69c3e679b6adb45736c373be1f5dd1c3dd7ac7d
oldguardmd <oldguard@msn.com>
2021-02-21 11:00:06 -0500

    Restructured reset all pins

--- Python/sam.lcd.test.v2.py
+++ Python/sam.lcd.test.v2.py
@@ -3,6 +3,9 @@ from time import sleep
 #We are using GPIO port numbers instead of pin numbers.
 
 class LCD:
+    pin_rs_cmd = False  # Equivelent of zero or off for register select.
+    pin_rs_data = True  # Equivelent of 1 or on for register select.
+
     def __init__(self, pin_e=15, pin_rs=13, pin_list=[22,18,16,12]):
         import RPi.GPIO as GPIO
         self.GPIO = GPIO
@@ -17,10 +20,14 @@ class LCD:
         self.full_pin_list.append(self.pin_e)
         self.full_pin_list.append(self.pin_rs)
     
-    def main(self):
+    def resetAllPins(self):
         for pin in self.full_pin_list:
-            print(pin)
             self.GPIO.setup(pin, self.GPIO.OUT)
+            sleep(.5)
+        return
+
+    def main(self):
+        self.resetAllPins()
             
 
 
