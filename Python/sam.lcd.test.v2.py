9c52a57a0206277731eebe179bd33784f39e52fc
oldguardmd <oldguard@msn.com>
2021-02-21 11:53:04 -0500

    New Method for pin list - test

--- Python/sam.lcd.test.v2.py
+++ Python/sam.lcd.test.v2.py
@@ -6,28 +6,44 @@ class LCD:
     pin_rs_cmd = False  # Equivelent of zero or off for register select.
     pin_rs_data = True  # Equivelent of 1 or on for register select.
 
-    def __init__(self, pin_e=15, pin_rs=13, pin_list=[22,18,16,12]):
+    #Control Pins
+    pin_rs = 13
+    pin_e = 15
+
+    #Data Pin Assignments
+    lcd_d4 = 22
+    lcd_d5 = 18
+    lcd_d6 = 16
+    lcd_d7 = 12
+
+    def __init__(self):
         import RPi.GPIO as GPIO
         self.GPIO = GPIO
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
-
-        self.pin_rs = pin_rs    #GPIO 27
-        self.pin_e = pin_e      #GPIO 22
-        self.pin_list = pin_list    #GPIO 25, 24, 23, 18
-
-        self.full_pin_list = self.pin_list[:]
-        self.full_pin_list.append(self.pin_e)
-        self.full_pin_list.append(self.pin_rs)
+        self.full_pin_list = {}
+        self.full_pin_list['e]'] = self.pin_e
+        self.full_pin_list['rs'] = self.pin_rs
+        self.full_pin_list['d4'] = self.lcd_d4
+        self.full_pin_list['d5'] = self.lcd_d5
+        self.full_pin_list['d6'] = self.lcd_d6
+        self.full_pin_list['d7'] = self.lcd_d7
+        print(self.full_pin_list)
+        exit()
     
-    def resetAllPins(self):
+    def setAllPinsToOutput(self):
         for pin in self.full_pin_list:
             self.GPIO.setup(pin, self.GPIO.OUT)
+        return
+
+    def resetAllPins(self):
+        for pin in self.full_pin_list:
+            #
             sleep(.5)
         return
 
     def main(self):
-        self.resetAllPins()
+        self.setAllPinsToOutput()
             
 
 
